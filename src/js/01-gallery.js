8815af60aac45f13c51d59d8b58cef6ba02b8fee
Karwan87 <damianuszynski@gmail.com>
2023-02-26 16:13:32 +0100

    Zadanie 1
    Tworzenie i renderowanie znacznika zgodnie z tablicą danych galleryItems i dostarczonym szablonem elementu galerii - wykonane.
    Implementacja delegowania na div.gallery i otrzymanie url większego obrazu - w toku, nie działa wyświetlanie większego obrazu - tu potrzebuję pomocy

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,28 @@
-import { galleryItems } from "./gallery-items.js";
+import { galleryItems, createGalleryItem } from "./gallery-items.js";
+const galleryMarkup = galleryItems.map(createGalleryItem).join("");
 // Change code below this line
+import * as basicLightbox from "https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.js";
 
+const galleryList = document.querySelector(".gallery");
+
+galleryList.insertAdjacentHTML("beforeend", galleryMarkup);
+
+galleryList.addEventListener("click", onGalleryClick);
+
+function onGalleryClick(event) {
+  event.preventDefault();
+
+  const isGalleryItem = event.target.classList.contains("gallery__image");
+  if (!isGalleryItem) {
+    return;
+  }
+
+  const imageUrl = onGalleryClick.dataset.source;
+
+  const instance = basicLightbox.create(`
+    <img src="${preview}" width="800" height="600">
+  `);
+
+  instance.show();
+}
 console.log(galleryItems);
--- src/js/gallery-items.js
+++ src/js/gallery-items.js
@@ -63,3 +63,17 @@ export const galleryItems = [
     description: "Lighthouse Coast Sea",
   },
 ];
+export const createGalleryItem = ({ preview, original, description }) =>
+  `<li class="gallery__item">
+    <a
+      class="gallery__link"
+      href="${original}"
+    >
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </li>`;
